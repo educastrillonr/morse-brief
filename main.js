5916fed978e172b753d2405f9fedf7cb84692fa7
Eduardo Romero <eduardo.romero@Nology.local>
2019-08-19 17:34:11 +0100

    donezo

--- main.js
+++ main.js
@@ -1,130 +1,97 @@
-function translateEng(message) {
+const morseRef = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  " ": "/"
+};
+
+const handleEnglish = e => {
+  const input = document.getElementById("englishInput");
+  document.getElementById("morseOutput").innerHTML = translateEng(input.value);
+};
+const handleMorse = e => {
+  const input = document.getElementById("morseInput");
+  document.getElementById("englishOutput").innerHTML = translateMor(
+    input.value
+  );
+};
+
+const translateEng = message => {
   let translation = "";
   for (let index = 0; index < message.length; index++) {
-    translation += getChar(message.toUpperCase().charAt(index));
+    translation += morseRef[message.toLowerCase().charAt(index)] + " ";
+    // console.log();
   }
-  console.log(translation);
+  return translation.substring(0, translation.length - 1);
+};
 
-  return translation;
-}
-
-function getChar(char) {
-  switch (char) {
-    case "A":
-      return ".- ";
-      break;
-    case "B":
-      return "-... ";
-      break;
-    case "C":
-      return "-.-. ";
-      break;
-    case "D":
-      return "-.. ";
-      break;
-    case "E":
-      return ". ";
-      break;
-    case "F":
-      return "..-. ";
-      break;
-    case "G":
-      return "--. ";
-      break;
-    case "H":
-      return ".... ";
-      break;
-    case "I":
-      return ".. ";
-      break;
-    case "J":
-      return ".--- ";
-      break;
-    case "K":
-      return "-.- ";
-      break;
-    case "L":
-      return ".-.. ";
-      break;
-    case "M":
-      return "-- ";
-      break;
-    case "N":
-      return "-. ";
-      break;
-    case "O":
-      return "--- ";
-      break;
-    case "P":
-      return ".--. ";
-      break;
-    case "Q":
-      return "--.- ";
-      break;
-    case "R":
-      return ".-. ";
-      break;
-    case "S":
-      return "... ";
-      break;
-    case "T":
-      return "- ";
-      break;
-    case "U":
-      return "..- ";
-      break;
-    case "V":
-      return "...- ";
-      break;
-    case "W":
-      return ".-- ";
-      break;
-    case "X":
-      return "-..- ";
-      break;
-    case "Y":
-      return "-.-- ";
-      break;
-    case "Z":
-      return "--.. ";
-      break;
-    case "0":
-      return "----- ";
-      break;
-    case "1":
-      return ".---- ";
-      break;
-    case "2":
-      return "..--- ";
-      break;
-    case "3":
-      return "...-- ";
-      break;
-    case "4":
-      return "....- ";
-      break;
-    case "5":
-      return "..... ";
-      break;
-    case "6":
-      return "-.... ";
-      break;
-    case "7":
-      return "--... ";
-      break;
-    case "8":
-      return "---.. ";
-      break;
-    case "9":
-      return "----. ";
-      break;
-    case " ":
-      return "/ ";
-      break;
+const translateMor = message => {
+  message += " ";
+  let translation = "";
+  let char = "";
+  for (let index = 0; index < message.length; index++) {
+    if (message.charAt(index) !== " ") {
+      char += message.toLowerCase().charAt(index);
+    } else {
+      translation += Object.keys(morseRef).find(key => morseRef[key] === char);
+      // console.log());
+      char = "";
+    }
   }
-  return translation;
-}
+  if (message === "") {
+    return " ";
+  }
+  return (
+    translation.substring(0, 1).toUpperCase() +
+    translation.substring(1, translation.length)
+  );
+};
 
 translateEng("Never gonna give you up");
+translateMor(
+  "-. . ...- . .-. / --. --- -. -. .- / --. .. ...- . / -.-- --- ..- / ..- .--."
+);
 
-// module.exports = { translateEng, getChar };
+module.exports = { translateEng, translateMor };
--- main.test.js
+++ main.test.js
@@ -1,9 +1,13 @@
 const main = require("./main");
 
-test("Get character and translate it to morse code & viceversa", function() {
-  expect(main.getChar("N")).toBe("-. ");
-  expect(main.getChar(" ")).toBe("/ ");
-});
+// test("Get character and translate it to morse code & viceversa", function() {
+//   expect(main.getMorseChar("N")).toBe("-.");
+//   expect(main.getMorseChar(" ")).toBe("/");
+//   expect(main.getEnglishChar("-.")).toBe("N");
+//   expect(main.getEnglishChar("/")).toBe(" ");
+// });
+
+//^ Test above became irrelevant after implementing morseRef
 
 test("Translate from morse code to english & viceversa", function() {
   expect(main.translateEng("Never gonna give you up")).toBe(
